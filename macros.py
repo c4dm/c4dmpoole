e70854280bcfb1ddd6ee096f64d75c06f4500996
Dan Stowell <danstowell@users.sourceforge.net>
2015-03-25 19:32:32 +0000

    fix typo which might have made the skip-tweets link fail

--- macros.py
+++ macros.py
@@ -1,7 +1,7 @@
 import csv
 
 def twitter():
-	return """<a href="#aftertw" id="skiptw">Skip tweets</a><a class="twitter-timeline" href="https://twitter.com/c4dm" data-widget-id="561187870955040769" data-chrome="nofooter" width="300" height="900">Tweets by @c4dm</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script><a name="#aftertw"></a>"""
+	return """<a href="#aftertw" id="skiptw">Skip tweets</a><a class="twitter-timeline" href="https://twitter.com/c4dm" data-widget-id="561187870955040769" data-chrome="nofooter" width="300" height="900">Tweets by @c4dm</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script><br /><a name="aftertw" id="aftertw"></a>"""
 
 
 def peoplelist(criteria=None):
