50f6b1752eca5cb4c40f172261e5d52af22ed19d
Dan Stowell <danstowell@users.sourceforge.net>
2015-03-26 15:49:56 +0000

    convert the "people" data from TSV to JSON - hopefully easier to maintain

--- macros.py
+++ macros.py
@@ -1,4 +1,5 @@
-import csv
+import csv, glob, json
+from operator import itemgetter
 
 def twitter():
 	return """<a href="#aftertw" id="skiptw">Skip tweets</a><a class="twitter-timeline" href="https://twitter.com/c4dm" data-widget-id="561187870955040769" data-chrome="nofooter" width="300" height="900">Tweets by @c4dm</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script><br /><a name="aftertw" id="aftertw"></a>"""
@@ -9,21 +10,28 @@ def peoplelist(criteria=None):
 		criteria = {}
 	ret = "<table width='100%' class='peoplelist'>"
 	ret += "\n  <tr><th>Name</th><th>Project/interests/keywords</th></tr>"
-	with open("input/people.tsv", "r") as fp:
-		rdr = csv.DictReader(fp, delimiter='\t')
-		for x in rdr:
-			if any([x.get(key, '') != val for key, val in criteria.items()]):
-				continue
-			if x.get('url', '') != '':
-				htmlname = "<a href='%s'>%s</a>" % (x['url'], x.get('name'))
-			else:
-				htmlname = x.get('name')
-			if x.get('acadposition', '') != '':
-				htmlname += "<br />%s" % (x.get('acadposition'))
-			blurb = x.get('blurb', '')
-			if blurb in [None, 'None', '*']:
-				blurb = ''
-			ret += "\n   <tr><td>%s</td><td>%s</td></tr>" % (htmlname, blurb)
+
+	peopledata = {}
+	for fpath in glob.glob("input/people_data/*.json"):
+		with open(fpath, 'rb') as jsonfp:
+			aperson = json.load(jsonfp)
+			aperson['sortkey'] = aperson['name'].strip().split(' ')[-1]
+			print aperson
+			peopledata[aperson['name']] = aperson
+
+	for x in sorted(peopledata.values(), key=itemgetter('sortkey')):
+		if any([x.get(key, '') != val for key, val in criteria.items()]):
+			continue
+		if x.get('url', '') != '':
+			htmlname = "<a href='%s'>%s</a>" % (x['url'], x.get('name'))
+		else:
+			htmlname = x.get('name')
+		if x.get('acadposition', '') != '':
+			htmlname += "<br />%s" % (x.get('acadposition'))
+		blurb = x.get('blurb', '')
+		if blurb in [None, 'None', '*']:
+			blurb = ''
+		ret += "\n   <tr><td>%s</td><td>%s</td></tr>" % (htmlname, blurb)
 	ret += "</table>"
 	return ret
 
